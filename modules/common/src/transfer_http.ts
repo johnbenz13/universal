470ec220fafa006ca52fb63513029d9a3e674274
Jonathan Bensaid <john@bensaidj.com>
2017-11-12 22:05:54 +0200

    Fix other stuff

--- modules/common/src/transfer_http.ts
+++ modules/common/src/transfer_http.ts
@@ -6,7 +6,7 @@ import {filter} from 'rxjs/operator/filter';
 import {first} from 'rxjs/operator/first';
 import {toPromise} from 'rxjs/operator/toPromise';
 import {_do} from 'rxjs/operator/do';
-import * as URL from 'url-parse';
+const URL = require('url-parse');
 
 import {BrowserTransferStateModule, TransferState, makeStateKey, StateKey} from '@angular/platform-browser';
 
@@ -33,13 +33,7 @@ export class TransferHttpCacheInterceptor implements HttpInterceptor {
 
   private makeCacheKey(req: HttpRequest<any>): StateKey<TransferHttpResponse> {
     const urlObject = new URL(req.urlWithParams);
-
-    console.log('URL OBJECT', urlObject);
-
     const urlString = urlObject.pathname + urlObject.query;
-
-    console.log('URL QUERY', urlObject.query, urlString);
-
     const headersString = JSON.stringify(req.headers);
 
     return makeStateKey<TransferHttpResponse>(urlString + headersString);
